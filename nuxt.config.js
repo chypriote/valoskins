1256d0e83a864eaf50b40a93140b9bc251bb7acf
Nicolas <chypriote@lolpros.gg>
2020-07-16 17:59:26 +0200

    first init

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+export default {
+	mode: 'universal',
+	target: 'static',
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: process.env.npm_package_description || '',
+			},
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+	css: [],
+	plugins: [],
+	components: true,
+	buildModules: [
+		'@nuxt/typescript-build',
+		'@nuxtjs/stylelint-module',
+		// Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+		'@nuxtjs/tailwindcss',
+	],
+	modules: [
+		'@nuxtjs/dotenv',
+		'@nuxtjs/axios',
+		'@nuxtjs/pwa',
+		'@nuxt/content',
+		'@nuxtjs/svg',
+	],
+	axios: {},
+	/*
+	 ** Content module configuration
+	 ** See https://content.nuxtjs.org/configuration
+	 */
+	content: {},
+	/*
+	 ** Build configuration
+	 ** See https://nuxtjs.org/api/configuration-build/
+	 */
+	build: {},
+}
