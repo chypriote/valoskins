20306ff090f33cb363f63356281961dd9d6ae4e4
Nicolas <chypriote@lolpros.gg>
2020-07-20 02:23:33 +0200

    a little of front

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app" class="custom-background bg-gray-100 text-gray-800 h-screen">
-		<div class="container mx-auto">
+		<div class="container mx-auto mb-8">
 			<navbar />
 		</div>
 		<div class="container mx-auto">
